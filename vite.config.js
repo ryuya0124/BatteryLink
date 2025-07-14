b31b30088fb63bfb43ffb2ccebf04372a0535e16
ryuya <ryuya1adgjmptw@gmail.com>
2025-07-14 19:08:43 +0900

    色々直した なぜかCSSが効いてない

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
